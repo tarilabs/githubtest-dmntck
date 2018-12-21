181bfee915018be3176d39293daed80dc7ccd679
tarilabs <matteo.mortari@gmail.com>
2018-12-21 14:55:08 +0100

    Test

--- action-publish/entrypoint.sh
+++ action-publish/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh -l
 
 echo "GITHUB_REF=$GITHUB_REF"
+echo "$GITHUB_REF"
 
-if  [[ $GITHUB_REF == refs/tags/publish* ]] ;
+if [ "${GITHUB_REF:0:17}" == "refs/tags/publish" ] ;
 then
   echo OK
+  export DMNTCK_PUBLISH_ID=${GITHUB_REF#"refs/tags/publish"}
+  echo "DMNTCK_PUBLISH_ID=$DMNTCK_PUBLISH_ID"
+  echo " "
   mvn --version
+  echo " "
   ls -la
 else
   echo ERROR the GITHUB_REF must start with refs/tags/publish
